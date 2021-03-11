a6a176f2fed9210b377650b48b998b4468379d82
Kmashaa <74793818+Kmashaa@users.noreply.github.com>
2021-03-11 22:29:46 +0300

    Add files via upload

---
+++ lab 2/2.1/2.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// Получить текущее время и дату в двух разных форматах и вывести на экран коли-чество нулей, единиц, …, девяток в их записи.
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Checking(string format)
+        {
+            int counter = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < format.Length; j++)
+                {
+                    if (format[j] == ((char)(i + 48)))
+                    {
+                        counter++;
+                    }
+                }
+                Console.Write(((char)(i + 48)) + "-" + counter + "     ");
+                counter = 0;
+            }
+        } 
+        static void Main()
+        {
+            DateTime now = DateTime.Now;
+            string firstFormat = now.ToString("F");
+            string secondFormat = now.ToString("G");
+           
+            Checking(firstFormat);
+            Console.WriteLine(firstFormat);
+
+            Checking(secondFormat);
+            Console.WriteLine(secondFormat);
+        }
+    }
+}
---
+++ lab 2/2.2/2.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//Дана строка. Записать номера символов, входящих в нее, в виде шестнадцатерич-ных чисел через пробелы. Числа должны быть ровно из четырех цифр (возможно, с нулями в начале).
+namespace _2._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            for (int i = 0; i < text.Length; i++)
+            {
+                int value = Convert.ToInt32(text[i]);
+                string numHex = $"{value:X}";
+                if (numHex.Length < 4)
+                {
+                    do
+                    {
+                        numHex = numHex.Insert(0, "0");
+                    } while (numHex.Length < 4);
+                }
+                Console.Write(numHex + " ");
+            }
+        }
+    }
+}
