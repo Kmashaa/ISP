468f30d0b90e40c1a627e9392a275e1341fc0191
Kmashaa <74793818+Kmashaa@users.noreply.github.com>
2021-04-23 06:51:14 +0300

    Update biathlon.cs

--- lab 5/ConsoleApp5/biathlon.cs
+++ lab 5/ConsoleApp5/biathlon.cs
@@ -13,22 +13,27 @@ namespace Biathlon
         Parameters p;
         protected string _accuracy { get; set; }
         protected string _normalSpeed { get; set; }
+        
         public override void GainWeight()
         {
             _weight++;
         }
+        
         public override void GainWeight(int moreWeight)
         {
             _weight += moreWeight;
         }
+        
         public override void LoseWeight()
         {
             _weight--;
         }
+        
         public override void LoseWeight(int lessWeight)
         {
             _weight -= lessWeight;
         }
+        
         public void NewAccuracy(string n)
         {
             _accuracy = CheckData(n, 47, 57);
@@ -36,19 +41,19 @@ namespace Biathlon
        
        public int[] shooting = new int[5] { 0, 0, 0, 0, 0 };
       
-        public void Shoot(int f, int s, int th, int ff, int fff)
-        {
+       public void Shoot(int f, int s, int th, int ff, int fff)
+       {
             shooting[0] = f;
             shooting[1] = s;
             shooting[2] = th;
             shooting[3] = ff;
             shooting[4] = fff;
             int num = 0;
-             for(int i = 0; i < 5; i++)
-             {
-                  num += shooting[i];
+            for(int i = 0; i < 5; i++)
+            {
+                 num += shooting[i];
 
-             }
+            }
         
             num *= 2;
             
@@ -56,6 +61,7 @@ namespace Biathlon
             _accuracy += "/10";
 
         }
+        
         public Biathlonist(string name, string surname, string age, string gender, string profession,
                       string salary, string endurance, int weight, string height,string accuracy , string normalSpeed)
         {
@@ -76,13 +82,12 @@ namespace Biathlon
             _accuracy = CheckData(accuracy,47,57);
             _normalSpeed = CheckData(normalSpeed,48,57);
         }
+        
         public override void Information()
         {
             Console.WriteLine($"\nName: {_name}\nSurname: {_surname}\nAge: {_age}\n" +
                 $"Gender: {_gender}\nProfession: runner\nSalary: {_salary}\nEndurance: {_endurance}\nWeight(kg): {_weight}\nHeight(cm): {_height}\n" +
                 $"Accuracy: {_accuracy}\nNormal speed(km/h): {_normalSpeed}\n");
         }
-
-
     }
 }
