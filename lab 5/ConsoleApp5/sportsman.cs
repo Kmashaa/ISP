e87a6bd83bb1b1d982b70967c113b5ff5a47fac7
Kmashaa <74793818+Kmashaa@users.noreply.github.com>
2021-04-23 06:49:20 +0300

    Add files via upload

---
+++ lab 5/ConsoleApp5/sportsman.cs
@@ -0,0 +1,15 @@
+﻿using Person;
+
+namespace Sport
+{
+    abstract class Sportsman: Human
+    {
+        protected string _endurance { get; set; }
+        protected int _weight { get; set; }
+        protected string _height { get; set; }
+        public abstract void GainWeight();
+        public abstract void GainWeight(int num);
+        public abstract void LoseWeight();
+        public abstract void LoseWeight(int num);
+    }
+}
